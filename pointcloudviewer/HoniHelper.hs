8336ecfa4bfa9eff5c69feb02c6d1ab4f6170786
Niklas Hambüchen <mail@nh2.me>
2014-04-05 19:11:16 +0100

    fixup! Initialize OpenNI only once (speeds up taking snapshots)

--- pointcloudviewer/HoniHelper.hs
+++ pointcloudviewer/HoniHelper.hs
@@ -20,18 +20,19 @@ takeDepthSnapshot :: IO (Either String (Vector Word16, (Int, Int)))
 takeDepthSnapshot = do
   -- A little bit dirty, but double initialization is allowed:
   -- https://github.com/OpenNI/OpenNI2/blob/33355e/Source/Core/OniContext.cpp#L50
-  initialize oniApiVersion
-
-  getDeviceList `orWarn` \case
-    []   -> return $ Left "No depth device"
-    di:_ -> deviceOpenInfo di `orWarn` \d -> do
-      deviceCreateStream d SensorDepth `orWarn` \stream -> do
-        streamStart stream >>= \case
-          s | s /= StatusOK -> return $ Left ("streamStart failed")
-          _                 -> do
-            streamReadFrame stream `orWarn` \OniFrame{ frameData, frameWidth, frameHeight } -> do
-              let frameVec = bsToVector16Bits frameData
-              return $ Right (frameVec, (frameWidth, frameHeight))
+  initialize oniApiVersion >>= \case
+    s | s /= StatusOK -> return $ Left ("initialize failed")
+    _                 -> do
+      getDeviceList `orWarn` \case
+        []   -> return $ Left "No depth device"
+        di:_ -> deviceOpenInfo di `orWarn` \d -> do
+          deviceCreateStream d SensorDepth `orWarn` \stream -> do
+            streamStart stream >>= \case
+              s | s /= StatusOK -> return $ Left ("streamStart failed")
+              _                 -> do
+                streamReadFrame stream `orWarn` \OniFrame{ frameData, frameWidth, frameHeight } -> do
+                  let frameVec = bsToVector16Bits frameData
+                  return $ Right (frameVec, (frameWidth, frameHeight))
 
   where
     orWarn :: Oni a -> (a -> IO (Either String b)) -> IO (Either String b)
