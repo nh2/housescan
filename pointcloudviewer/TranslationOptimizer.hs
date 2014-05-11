67d810bbe7ae43a1d50e45ba141b1c208df8bd99
Niklas Hambüchen <mail@nh2.me>
2014-05-11 17:34:20 +0100

    TranslationOptimizer: Allow arbitraty indices, biject them to [0..n-1]

--- pointcloudviewer/TranslationOptimizer.hs
+++ pointcloudviewer/TranslationOptimizer.hs
@@ -1,16 +1,43 @@
 {-# LANGUAGE MultiWayIf #-}
 
+module TranslationOptimizer
+  ( lstSqDistances
+  ) where
+
 import Data.Map (Map)
+import Data.Maybe
 import qualified Data.Map as Map
+import qualified Data.Set as Set
 import Data.Packed.Matrix
 import Data.Packed.Vector
 import Numeric.LinearAlgebra.Algorithms (linearSolveLS)
 
-import Debug.Trace
+
+biject :: (Ord a) => [a] -> (a -> Int, Int -> a)
+biject xs = (indexOf, aOfIndex)
+  where
+    uniqueAs = Set.toList . Set.fromList $ xs
+    indexToAMap = Map.fromList $ zip [(0::Int)..] uniqueAs
+    aToIndexMap = Map.fromList $ zip uniqueAs     [0..]
+    get x = fromMaybe (error "biject") . Map.lookup x
+    indexOf x = get x aToIndexMap
+    aOfIndex x = get x indexToAMap
 
 
-lstSqDistances :: Map (Int, Int) Double -> [Double]
-lstSqDistances distMap = points
+
+lstSqDistances :: (Ord a) => Map (a, a) Double -> Map a Double
+lstSqDistances distMap = res
+  where
+    uniq = Set.toList . Set.fromList
+    uniqueAs = uniq $ concat [ [a,b] | (a,b) <- Map.keys distMap ]
+    (indexOf, aOfIndex) = biject uniqueAs
+
+    pos = lstSqDistancesI (Map.mapKeys (\(a,b) -> (indexOf a, indexOf b)) distMap)
+    res = Map.fromList $ zip (map aOfIndex [0..]) pos
+
+
+lstSqDistancesI :: Map (Int, Int) Double -> [Double]
+lstSqDistancesI distMap = points
   where
     dists = Map.toList distMap
     n = succ . maximum $ concat [ [i,j] | ((i,j),_) <- dists ]
